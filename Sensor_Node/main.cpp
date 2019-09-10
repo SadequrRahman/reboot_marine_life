0e2ecf977bf23bc67e56b1ec039a17c5b02d0ad8
sadequr rahman rabby <sadequr.rahman.rabby@gmail.com>
2019-09-10 14:45:28 +0200

    init commit

---
+++ Sensor_Node/main.cpp
@@ -0,0 +1,22 @@
+/*
+	File : main.c
+	Author : mohammad sadequr Rahman 
+	Date : 10/09/2019
+
+	Desription: This is intended to simulate a sensor node
+				for UNTIL project. It will generate stimulus
+				and send this data point to server.
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	cout << "Hello Worrld" << endl;
+
+	return 0;
+}
